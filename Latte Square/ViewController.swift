faa87f778f15f33240c5a0860fb267be6a741531
Raphat <raphat@livingmobile.me>
2023-02-05 17:47:10 +0700

    first commit

---
+++ Latte Square/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Latte Square
+//
+//  Created by MacbookAir M1 FoodStory on 5/2/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
