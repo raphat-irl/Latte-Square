178e3eda9d544226a6b2cbe098ed4a2caa6b80cc
Raphat <raphat@livingmobile.me>
2023-02-05 17:49:43 +0700

    pod install

---
+++ Pods/Target Support Files/Pods-Latte Square-Latte SquareUITests/Pods-Latte Square-Latte SquareUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Latte_Square_Latte_SquareUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Latte_Square_Latte_SquareUITests
+@end
---
+++ Pods/Target Support Files/Pods-Latte SquareTests/Pods-Latte SquareTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Latte_SquareTests : NSObject
+@end
+@implementation PodsDummy_Pods_Latte_SquareTests
+@end
