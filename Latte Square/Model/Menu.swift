6f444f2c1526b2998c5f08f3aaa1a1e25094097c
Raphat <raphat@livingmobile.me>
2023-02-07 13:07:43 +0700

    setup Model

---
+++ Latte Square/Model/Menu.swift
@@ -0,0 +1,17 @@
+//
+//  Menu.swift
+//  Latte Square
+//
+//  Created by MacbookAir M1 FoodStory on 7/2/2566 BE.
+//
+
+import Foundation
+
+struct Menu {
+    
+    var title: String
+    var desc: String
+    var price: Int
+    var image: String
+    
+}
